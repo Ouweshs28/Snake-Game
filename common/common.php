bde1e6ba2b5c0a03922adcd77780d9714869e103
Ouweshs28 <ouweshseeroo@gmail.com>
2019-11-02 23:52:17 +0400

    Initial PHP Home Page

---
+++ common/common.php
@@ -0,0 +1,75 @@
+<?php
+
+//Outputs common header
+function generateHeader($pageTitle,$pagecss){
+    echo '<!DOCTYPE html>';
+    echo '<html lang="en">';
+    echo '<head>';
+    echo '<meta charset="utf-8">';
+    echo '<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->';
+    echo '<title>' . $pageTitle . '</title>';
+    echo '<!-- Bootstrap -->';
+    echo '<link href="common/css/bootstrap/bootstrap.min.css" rel="stylesheet">';
+    echo '<!-- Common CSS -->';
+    echo '<link href="common/css/common.css" rel="stylesheet" type="text/css">';
+    echo '<!-- '. $pageTitle .'CSS -->';
+    echo '<link href="css/'.$pagecss.'-style.css" rel="stylesheet" type="text/css">';
+    echo ' <!-- Snake Fav ICON -->';
+    echo '<link rel="shortcut icon" type="image/png" href="common/img/snake_icon.ico"/>';
+}
+
+//Closes head
+
+function closeHead(){
+    echo '</head>';
+    echo '<body>';
+}
+
+
+/* Outputs NavigationBar with corresponding link */
+function outputNavBar($pageName){
+   //  Common Navigation Part
+    echo '<!--Navigation Bar-->';
+    echo '<nav class="navbar fixed-top" id="navBar">';
+    echo '<a class="navbar-brand" href="index.html">
+        <img alt="snake icon" class="d-inline-block align-top" src="common/img/snake-icon.png"> Snake
+    </a>';
+    echo '<ul class="navbar-nav ml-auto">
+        <li class="nav-item">';
+    
+    //Array of pages to link to
+    $linkNames = array("Home", "Score", "Login");
+    $linkFolderHome = array( "","score/", "login/");
+    $linkFolder = array("../", "../score/", "../login/");
+    $linkFiles = array("index.php", "score.php", "login.php");
+    
+
+    for($x = 0; $x < count($linkNames); $x++){
+        echo '<a class="btn navbar-btn navButton" type="button"';
+        if($linkNames[$x] == $pageName){
+            echo 'id="activeNavBtn';
+        }
+        if ($pageName=='Home'){
+            if($x==0){
+                echo 'href="' . $linkFiles[$x] . '">' . $linkNames[$x] . '</a>';
+            }else{
+
+            echo 'href="' . $linkFolderHome[$x] . $linkFiles[$x]. '">' . $linkNames[$x] . '</a>';
+            }
+        }else{
+            echo 'href="' . $linkFolder[$x] . $linkFiles[$x]. '">' . $linkNames[$x] . '</a>';
+        }
+    }
+    echo '</li>
+    </ul>
+</nav>';
+}
+
+function generateCommonJS(){
+    echo '<!-- jQuery (necessary for Bootstrap\'s JavaScript plugins) -->';
+    echo '<script src="common/js/jquery/jquery.min.js"></script>';
+    echo '<!-- Include all compiled plugins (below), or include individual files as needed -->';
+    echo '<script src="common/js/bootstrap/bootstrap.min.js"></script>';
+    echo '</html>';
+}
+
---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+//Include the PHP functions to be used on the page
+include('common/common.php');
+
+//Output header and navigation
+generateHeader("Snake -Home","home");
+closeHead();
+outputNavBar("Home");
+?>
+<!-- Carousel -->
+<div class="carousel slide" data-ride="carousel" id="carouselGameplayIndicators">
+    <ol class="carousel-indicators">
+        <li class="active" data-slide-to="0" data-target="#carouselGameplayIndicators"></li>
+        <li data-slide-to="1" data-target="#carouselGameplayIndicators"></li>
+        <li data-slide-to="2" data-target="#carouselGameplayIndicators"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img alt="First slide" class="d-block" src="img/gameplay1.PNG">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Some temporary screenshots of a snake game</h5>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img alt="Second slide" class="d-block" src="img/gameplay2.png">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Some temporary screenshots of a snake game</h5>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img alt="Third slide" class="d-block" src="img/gameplay3.png">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Some temporary screenshots of a snake game</h5>
+            </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev" data-slide="prev" href="#carouselGameplayIndicators" role="button">
+        <span aria-hidden="true" class="carousel-control-prev-icon"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" data-slide="next" href="#carouselGameplayIndicators" role="button">
+        <span aria-hidden="true" class="carousel-control-next-icon"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+<!-- About Game Card Box -->
+<div class="card mb-3" id="aboutGame">
+    <div class="card-body card border-dark mb-3 " id="breifBox">
+        <h5 class="card-title">About Our Game</h5>
+        <p class="card-text">Play the classic Snake game for free at our website. You can register for free and challenge your friends. We have introduced some new game modes for a more challenging experience. You can keep track of your scores with your friends</p>
+    </div>
+</div>
+<!-- Center Play Button -->
+<div id="playButton" class="col text-center">
+    <a class="btn btn-primary col-md-2" type="button" href="play/play.html">Play</a>
+</div>
+<!-- Cards section  -->
+<div class="card-deck">
+    <div class="card">
+        <img class="card-img-top" src="img/registration-icon.png" alt="Register">
+        <div class="card-body">
+            <h5 class="card-title text-center">Registration</h5>
+            <p class="card-text">Sign up to keep track of your best scores and challenge yourself.</p>
+        </div>
+        <div class="card-footer">
+            <div class="col text-center">
+                <a class="btn btn-primary" type="button" href="register/register.html">Register now</a>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <img class="card-img-top" src="img/help-icon.png" alt="Help">
+        <div class="card-body">
+            <h5 class="card-title text-center">Help</h5>
+            <p class="card-text">Need help about the game? How to play, controls or information about the game? </p>
+        </div>
+        <div class="card-footer">
+            <div class="col text-center">
+                <a class="btn btn-primary" type="button" href="help/help.html">Find out more</a>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <img class="card-img-top" src="img/ranking.png" alt="Rankings">
+        <div class="card-body">
+            <h5 class="card-title text-center">Rankings</h5>
+            <p class="card-text">See your scores and compare them with other players. Beat your friends or your own best</p>
+        </div>
+        <div class="card-footer">
+            <div class="col text-center">
+                <a class="btn btn-primary" type="button" href="score/score.html">View top players</a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<footer>
+
+</footer>
+<?php
+generateCommonJS();
+?>
